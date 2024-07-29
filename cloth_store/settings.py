9128f777539f6269c1e46970fe77547e17c2884c
salauddin85 <ahmedsalauddin677785@gmail.com>
2024-07-29 23:28:15 +0600

    all origin added

--- cloth_store/settings.py
+++ cloth_store/settings.py
@@ -18,7 +18,7 @@ environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+CORS_ALLOW_ALL_ORIGINS = True
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
