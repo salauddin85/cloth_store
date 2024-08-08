67593a1ec06006c9dc762e0a9b8ca74d7c3458e7
salauddin85 <ahmedsalauddin677785@gmail.com>
2024-08-08 15:04:21 +0600

    origin added

--- cloth_store/settings.py
+++ cloth_store/settings.py
@@ -30,8 +30,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
-# CSRF_TRUSTED_ORIGINS = ['https://cloth-store-hipo.onrender.com']
-CSRF_TRUSTED_ORIGINS = ["https://cloth-store-hipo.onrender.com","https://*.127.0.0.1"] # new
+CSRF_TRUSTED_ORIGINS = ['https://cloth-store-hipo.onrender.com']
+# CSRF_TRUSTED_ORIGINS = ["https://cloth-store-hipo.onrender.com","https://*.127.0.0.1"] # new
 
 
 
