10eb505681b33a70882f715b88520d657221e95b
salauddin85 <ahmedsalauddin677785@gmail.com>
2024-08-08 22:12:35 +0600

    done

--- cloth_store/settings.py
+++ cloth_store/settings.py
@@ -29,7 +29,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = ['https://cloth-store-hipo.onrender.com', '0.0.0.0', 'localhost']
+ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = ['https://cloth-store-hipo.onrender.com']
 # CSRF_TRUSTED_ORIGINS = ["https://cloth-store-hipo.onrender.com","https://*.127.0.0.1"] # new
 
